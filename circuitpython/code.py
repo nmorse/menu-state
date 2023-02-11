5ce9ebf6149913b8d947aec107110c0d68b87a0a
nmorse <morse.nate@gmail.com>
2023-02-11 18:40:33 -0500

    button fix

--- circuitpython/code.py
+++ circuitpython/code.py
@@ -6,11 +6,11 @@ from buttons import catch_pin_transitions
 
 async def main():
     async_button_a = asyncio.create_task(catch_pin_transitions(board.A3, True, "button_a"))
-    async_button_a = asyncio.create_task(catch_pin_transitions(board.D2, False, "button_b"))
+    async_button_b = asyncio.create_task(catch_pin_transitions(board.D2, False, "button_b"))
     async_statemachine = asyncio.create_task(xstate_interpreter(menu_states))
     async_time_out = asyncio.create_task(time_out_click())
     await asyncio.gather(async_button_a, 
-                         async_button_a, 
+                         async_button_b, 
                          async_statemachine, 
                          async_time_out)
 
